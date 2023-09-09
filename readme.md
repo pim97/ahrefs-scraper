156f15445b15a0b1cff9fc29cc88a79043f51b39
pim de bree <pim_debree@hotmail.com>
2023-09-09 10:13:06 +0200

    .

---
+++ readme.md
@@ -0,0 +1,34 @@
+# Thefork.it Scraper
+
+[![License](https://img.shields.io/badge/license-MIT-blue.svg)](LICENSE)
+
+## Description
+
+Ahrefs is a powerful and widely-used SEO (Search Engine Optimization) tool and digital marketing platform. It offers a comprehensive suite of features that help website owners, marketers, and SEO professionals analyze and improve their online presence. Ahrefs provides tools for backlink analysis, keyword research, site auditing, competitor analysis, and more. It helps users understand their website's performance in search engines, identify opportunities for improvement, and track their SEO progress. Ahrefs is an essential resource for optimizing websites and increasing online visibility.
+
+## Installation
+
+1. Clone the repository: `git clone https://github.com/pim97/ahrefs-scraper.git`
+2. Install the dependencies: `npm install`
+
+## Usage
+
+1. Replace the following details with your own details in the script file (`scrape.js`):
+   - API_KEY_HERE: Your Scrappey API key found at https://app.scrappey.com/#/ to solve Cloudflare
+2. Run the script: `node scrape.js`
+
+## Contributing
+
+Contributions are welcome! Please follow these guidelines when contributing to the project.
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for more information.
+
+## Contact
+
+- Author: Pim97
+- GitHub: [Your GitHub Profile](https://github.com/pim97/)
+
+Keywords for Ahrefs scraping Program on GitHub:
+Ahrefs.com, SEO tools, Backlink analysis, Keyword research, Site audit, Competitor analysis, Rank tracking, Content research, Link building, SEO software, Website analytics, Keyword ranking, On-page SEO, Off-page SEO, Organic search, SEO strategy, Digital marketing, SEO audit, SERP analysis, SEO optimization, Website ranking.
---
+++ scrape.js
@@ -0,0 +1,73 @@
+const Scrappey = require('scrappey-wrapper');
+
+/**
+ * Check out our documentation here for more information: https://wiki.scrappey.com/
+ * Your key can be found here: https://app.scrappey.com/#/
+ */
+const SCRAPPEY_API_KEY = 'API_KEY';
+const scrappey = new Scrappey(SCRAPPEY_API_KEY);
+
+/**
+ * Scrappey.com is a proxy-wrapper for browsers, it allows you to run browser actions and execute javascript on any website.
+ * with advanced options such as caching, proxy rotation, anti-bot and more.
+ */
+async function run() {
+
+    /**
+     * For all session options check: https://wiki.scrappey.com/getting-started#78f3fd5551724a78b12d548e95485bbe
+     * We allow for multiple sessions to be created, each session has a different proxy and user-agent and unique fingerprint.
+     */
+    const session = await scrappey.createSession({
+        "datacenter": true,
+    })
+
+    /**
+     * Executes the browser actions requested
+     */
+    const result = await scrappey.get({
+        "cmd": "request.get",
+        "url": "https://ahrefs.com/backlink-checker?input=yep.com",
+        "interceptFetchRequest": "https://ahrefs.com/v4/stGetFreeBacklinksList",
+        "browserActions": [
+            {
+                "type": "click",
+                "cssSelector": "button[class='css-ljl0n3-button css-rxvlhs-buttonAlignContent css-ohx8vg-buttonColor css-8i44dk css-xja2l0-buttonFlexGrow css-r0mgdk']"
+            },
+            {
+                "type": "solve_captcha",
+                "captcha": "turnstile"
+            }
+        ]
+    })
+
+    /**
+     * Example response
+     * 
+     * {
+    solution: {
+        verified: true,
+        title: 'Free Backlink Checker by Ahrefs: Check Backlinks to Any Site',
+        userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/116.0',
+        innerText: '',
+        localStorageData: {},
+        cookies: [ [Object], [Object] ],
+        response: '["TopBacklinks",{"topBacklinks":{"backlinks":[{"anchor":"Learn more about Yep","domainRating":91,"edu":false,"gov":false,"httpCode":200,"redirectChain":[],"text":true,"textPost":"","textPre":"","title":"Ahrefs - SEO Tools & Resources To Grow Your Search Traffic","urlFrom":"https://ahrefs.com/","urlTo":"https://yep.com/about","inRendered":false,"inRaw":true,"redirectCode":0,"original":true,"urlToMustBeGray":false,"urlToHttpCodeCross":false,"urlToNew":"None","urlToChainDestinationChanged":false,"urlToDelReasonTitle":"None","urlToDelReasonKey":"None","urlToHasDelReason":false,"lost_redirect_reason":"","del_reason":"","lost_redirect_source":"","lost_redirect_new_target":"","isLost":true},{"anchor":"Yep.com","domainRating":91,"edu":false,"gov":false,"httpCode":200,"redirectChain":[],"text":true,"textPost":"","textPre":"2.","title":"21 Great Search Engines You Can Use Instead Of Google","urlFrom":"https://www.searchenginejournal.com/alternative-search-engines/271409/","urlTo":"https://yep.com/","inRendered":true,"inRaw":true,"redirectCode":0,"original":true,"urlToMustBeGray":false,"urlToHttpCodeCross":false,"urlToNew":"None","urlToChainDestinationChanged":false,"urlToDelReasonTitle":"None","urlToDelReasonKey":"None","urlToHasDelReason":false,"lost_redirect_reason":"","del_reason":"","lost_redirect_source":"","lost_redirect_new_target":"","isLost":true},{"anchor":"90% of its ad revenue","domainRating":93,"edu":false,"gov":false,"httpCode":200,"redirectChain":[{"url":"https://yep.com/about","urlNew":"None","redirectCode":308,"mustBeGray":false,"httpCodeCross":false,"chainDestinationChanged":false,"hasDelReason":false,"delReasonTitle":"None","delReasonKey":"None","lostRedirectReason":"None","isLost":true}],"text":true,"textPost":"with content creators. With this model, Yep aims to encourage","textPre":"beta, Yep, is unique from other platforms as it shares","title":"The Top 11 Search Engines, Ranked by Popularity","urlFrom":"https://blog.hubspot.com/marketing/top-search-engines","urlTo":"https://yep.com/about/","inRendered":true,"inRaw":true,"redirectCode":0,"original":true,"urlToMustBeGray":false,"urlToHttpCodeCross":false,"urlToNew":"None","urlToChainDestinationChanged":false,"urlToDelReasonTitle":"None","urlToDelReasonKey":"None","urlToHasDelReason":false,"lost_redirect_reason":"","del_reason":"","lost_redirect_source":"","lost_redirect_new_target":"","isLost":true},{"anchor":"Yep","domainRating":78,"edu":false,"gov":false,"httpCode":200,"redirectChain":[],"text":true,"textPost":", the latest search engine that rewards and compensates content","textPre":"","title":"7 Best AI Search Engines to Use as Google Alternatives","urlFrom":"https://geekflare.com/best-ai-search-engines/","urlTo":"https://yep.com/","inRendered":true,"inRaw":true,"redirectCode":0,"original":true,"urlToMustBeGray":false,"urlToHttpCodeCross":false,"urlToNew":"None","urlToChainDestinationChanged":false,"urlToDelReasonTitle":"None","urlToDelReasonKey":"None","urlToHasDelReason":false,"lost_redirect_reason":"","del_reason":"","lost_redirect_source":"","lost_redirect_new_target":"","isLost":true},{"anchor":"Yep","domainRating":90,"edu":false,"gov":false,"httpCode":200,"redirectChain":[],"text":true,"textPost":"comes with multiple AI capabilities that make the search experience","textPre":"","title":"Best AI tools of 2023 | TechRadar","urlFrom":"https://www.techradar.com/best/best-ai-tools","urlTo":"https://yep.com/","inRendered":true,"inRaw":true,"redirectCode":0,"original":true,"urlToMustBeGray":false,"urlToHttpCodeCross":false,"urlToNew":"None","urlToChainDestinationChanged":false,"urlToDelReasonTitle":"None","urlToDelReasonKey":"None","urlToHasDelReason":false,"lost_redirect_reason":"","del_reason":"","lost_redirect_source":"","lost_redirect_new_target":"","isLost":true},{"anchor":"32. YEP","domainRating":63,"edu":false,"gov":false,"httpCode":200,"redirectChain":[],"text":true,"textPost":"","textPre":"","title":"Top 10 Search Engines in the World + 46 Google Alternatives","urlFrom":"https://www.stanventures.com/blog/top-search-engines-list/","urlTo":"https://yep.com/","inRendered":false,"inRaw":true,"redirectCode":0,"original":true,"urlToMustBeGray":false,"urlToHttpCodeCross":false,"urlToNew":"None","urlToChainDestinationChanged":false,"urlToDelReasonTitle":"None","urlToDelReasonKey":"None","urlToHasDelReason":false,"lost_redirect_reason":"","del_reason":"","lost_redirect_source":"","lost_redirect_new_target":"","isLost":true},{"anchor":"Yep.com","domainRating":93,"edu":false,"gov":false,"httpCode":200,"redirectChain":[],"text":true,"textPost":"","textPre":"","title":"Liste von Websuchmaschinen – Wikipedia","urlFrom":"https://de.wikipedia.org/wiki/Liste_von_Websuchmaschinen","urlTo":"https://yep.com/","inRendered":false,"inRaw":true,"redirectCode":0,"original":true,"urlToMustBeGray":false,"urlToHttpCodeCross":false,"urlToNew":"None","urlToChainDestinationChanged":false,"urlToDelReasonTitle":"None","urlToDelReasonKey":"None","urlToHasDelReason":false,"lost_redirect_reason":"","del_reason":"","lost_redirect_source":"","lost_redirect_new_target":"","isLost":true},{"anchor":"not to store","domainRating":84,"edu":false,"gov":false,"httpCode":200,"redirectChain":[{"url":"https://yep.com/privacy","urlNew":"None","redirectCode":308,"mustBeGray":false,"httpCodeCross":false,"chainDestinationChanged":false,"hasDelReason":false,"delReasonTitle":"None","delReasonKey":"None","lostRedirectReason":"None","isLost":true}],"text":true,"textPost":"users’ search history or IP addresses.","textPre":"encourage new creators to join the space. Yep also claims","title":"15 Top Search Engines to Try | Built In","urlFrom":"https://builtin.com/consumer-tech/search-engines-list","urlTo":"https://yep.com/privacy/","inRendered":true,"inRaw":true,"redirectCode":0,"original":true,"urlToMustBeGray":false,"urlToHttpCodeCross":false,"urlToNew":"None","urlToChainDestinationChanged":false,"urlToDelReasonTitle":"None","urlToDelReasonKey":"None","urlToHasDelReason":false,"lost_redirect_reason":"","del_reason":"","lost_redirect_source":"","lost_redirect_new_target":"","isLost":true},{"anchor":"Yep","domainRating":40,"edu":false,"gov":false,"httpCode":200,"redirectChain":[],"text":true,"textPost":"","textPre":"","title":"Buscadores de Internet: ¿Qué son?, y cómo funcionan.","urlFrom":"https://www.seoenmexico.com.mx/blog/buscadores-de-internet/","urlTo":"https://yep.com/","inRendered":false,"inRaw":true,"redirectCode":0,"original":true,"urlToMustBeGray":false,"urlToHttpCodeCross":false,"urlToNew":"None","urlToChainDestinationChanged":false,"urlToDelReasonTitle":"None","urlToDelReasonKey":"None","urlToHasDelReason":false,"lost_redirect_reason":"","del_reason":"","lost_redirect_source":"","lost_redirect_new_target":"","isLost":true},{"anchor":"Yep","domainRating":61,"edu":false,"gov":false,"httpCode":200,"redirectChain":[],"text":true,"textPost":"は、SEOツールでお馴染みのAhrefs（エイチレフス）が開発したとあってか、各所で耳目を集めていま
+        す。特徴は、レベニューシェア型であること。コンテンツ配信者やクリエイターに9割の広告収入を渡しているといいます。比較的、Googleの検索結果に近い印象を覚えつつ、実際は、GoogleのAPIを使わずに、自社のインデ
+        ックスやデータセンターを運用しているようです。","textPre":"","title":"検索エンジンの種類を一覧で比較！おすすめや最新情報も紹介","urlFrom":"https://www.sungrove.co.jp/search-engine/","urlTo":"https://yep.com/","inRendered":true,"inRaw":true,"redirectCode":0,"original":true,"urlToMustBeGray":false,"urlToHttpCodeCross":false,"urlToNew":"None","urlToChainDestinationChanged":false,"urlToDelReasonTitle":"None","urlToDelReasonKey":"None","urlToHasDelReason":false,"lost_redirect_reason":"","del_reason":"","lost_redirect_source":"","lost_redirect_new_target":"","isLost":true},{"anchor":"YEP","domainRating":48,"edu":false,"gov":false,"httpCode":200,"redirectChain":[],"text":true,"textPost":", запущен в июне 2022 года основателем популярного SEO-инструмента Ahrefs","textPre":"","title":"Поисковые системы Украины и мира: 50+ альтернатив гиганту Google • Marketer","urlFrom":"https://marketer.ua/ru/rating-of-search-engines-of-ukraine-and-the-world-2022/","urlTo":"https://yep.com/","inRendered":false,"inRaw":true,"redirectCode":0,"original":true,"urlToMustBeGray":false,"urlToHttpCodeCross":false,"urlToNew":"None","urlToChainDestinationChanged":false,"urlToDelReasonTitle":"None","urlToDelReasonKey":"None","urlToHasDelReason":false,"lost_redirect_reason":"","del_reason":"","lost_redirect_source":"","lost_redirect_new_target":"","isLost":true},{"anchor":"Yep","domainRating":53,"edu":false,"gov":false,"httpCode":200,"redirectChain":[{"url":"http://yep.com/","urlNew":"None","redirectCode":308,"mustBeGray":false,"httpCodeCross":false,"chainDestinationChanged":false,"hasDelReason":false,"delReasonTitle":"None","delReasonKey":"None","lostRedirectReason":"None","isLost":true}],"text":true,"textPost":"","textPre":"","title":"Is English Easy to Learn? – RealLife English","urlFrom":"https://reallifeglobal.com/is-english-easy-to-learn/","urlTo":"https://yep.com/","inRendered":true,"inRaw":false,"redirectCode":0,"original":true,"urlToMustBeGray":false,"urlToHttpCodeCross":false,"urlToNew":"None","urlToChainDestinationChanged":false,"urlToDelReasonTitle":"None","urlToDelReasonKey":"None","urlToHasDelReason":false,"lost_redirect_reason":"","del_reason":"","lost_redirect_source":"","lost_redirect_new_target":"","isLost":true},{"anchor":"social","domainRating":12,"edu":false,"gov":false,"httpCode":200,"redirectChain":[{"url":"http://yep.com/","urlNew":"None","redirectCode":308,"mustBeGray":false,"httpCodeCross":false,"chainDestinationChanged":false,"hasDelReason":false,"delReasonTitle":"None","delReasonKey":"None","lostRedirectReason":"None","isLost":true}],"text":true,"textPost":"creatures, and they love nothing more than getting some good","textPre":"that hair is a great source of attention. Dogs are","title":"Why Does My Dog Bite My Hair? (8 Reasons)","urlFrom":"https://lovedoges.com/why-does-my-dog-bite-my-hair-8-reasons/","urlTo":"https://yep.com/","inRendered":false,"inRaw":true,"redirectCode":0,"original":true,"urlToMustBeGray":false,"urlToHttpCodeCross":false,"urlToNew"'... 68472 more characters
+        },
+        timeElapsed: 24205,
+        data: 'success',
+        session: 'f9a47571-a936-466c-81fc-74a440fbf4ac'
+        }
+    
+     */
+    console.log(result)
+
+
+    /**
+     * Destroys the session, this will free up space for other users
+     */
+    await scrappey.destroySession(session.session)
+}
+
+run().catch(console.error);
